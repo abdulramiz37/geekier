b3b238e195e55e02779403128a01cccaa46fafc4
abdul <abdulramiz007@gmail.com>
2022-07-08 09:27:29 +0530

    first co

--- src/Components/ArtistInfo.js
+++ src/Components/ArtistInfo.js
@@ -16,7 +16,7 @@ const ArtistInfo = ({ artistDetails }) => {
     <>
       <div className='h-52 sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/4 z-0 -mt-4 blur-sm' style={{ backgroundImage: `url('${getArtistImage(artistDetails.id)}')`, backgroundSize: "cover" }}></div>
       <div className="container mx-auto px-4 -mt-40 z-2 relative">
-        <img src={getArtistImage(artistDetails.id)} className="p-8 sm:w-1/2 sm:-ml-2 md:-ml-3 sm:-mt-4 md:w-1/3 lg:w-1/3 xl:w-1/4 sm:inline" />
+        <img src={getArtistImage(artistDetails.id)} className="p-8 sm:w-1/2 sm:-ml-2 md:-ml-3 sm:-mt-4 md:w-1/3 lg:w-1/3 xl:w-1/4 sm:inline" alt=""/>
 
         <div className='sm:inline-block sm:p-8 sm:w-2/5'>
           <p className='text-3xl font-semibold inline -mt-7 sm:mt-0 text-center sm:text-left'>{artistDetails.name}</p>
@@ -30,4 +30,4 @@ const ArtistInfo = ({ artistDetails }) => {
   )
 }
 
-export default ArtistInfo
+export default ArtistInfo 
